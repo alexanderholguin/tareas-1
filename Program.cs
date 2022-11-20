02e10df8ff2ef6849a5631bef8e78768cdda9412
alexanderholguin <118689700+alexanderholguin@users.noreply.github.com>
2022-11-20 17:49:39 -0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,21 +1,28 @@
-﻿internal class NewBaseType
+﻿/* nombre: fernanjps3A       */
+class Program
 {
     static void Main(string[] args)
     {
-        int num = 0;
-        int suma = 0;
-
-        do
+        double n1, n2, n3;
+        Console.WriteLine("primer lado:");
+        n1 = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("segundo lado:");
+        n2 = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("tercer lado:");
+        n3 = Convert.ToDouble(Console.ReadLine());
+        if (n1 == n2 && n1==n3)
+            Console.WriteLine("es equilatero");
+        else    
         {
-            Console.WriteLine("ingresar un numero:");
-            num = int.Parse(Console.ReadLine());
-            suma = suma + num;
-        } while (num != 0);
-        Console.WriteLine("la suma es:" + suma);
-        Console.ReadKey();
-    }
-}
+            if (n1== n2 ||n1 ==n3 || n3==n2)
+                Console.WriteLine("es isosceles");
+            else
+            {
+                if (n1 != n2 || n2!=n3 || n3 != n1)
+                    Console.WriteLine("es escaleno");
+            }    
+        }
+        Console.Read();
 
-class Program : NewBaseType
-{
+    }
 }
