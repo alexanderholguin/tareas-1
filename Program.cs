6fbd0c21248d9b3487c03afdd7fa3e20d1d9d93a
alexanderholguin <118689700+alexanderholguin@users.noreply.github.com>
2022-11-20 16:11:36 -0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* nombre: jonathanholguinbaque 3 A */
+internal class NewBaseType
+{
+    static void Main(string[] args)
+    {
+        double n1;
+        double n2;
+        Console.WriteLine("numero uno;");
+        n1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("numero dos:");
+        n2 = Convert.ToDouble(Console.ReadLine());
+
+        if (n1 > n2)
+        {
+            Console.WriteLine("el numero" + n1 + "es el mayor");
+        }
+        else if (n1 < n2)
+        {
+            Console.WriteLine("el numeros " + n2 + "es el mayor");
+        }
+        else
+        {
+            Console.WriteLine("los numeros son iguales.");
+        }
+        Console.ReadLine();
+    }
+}
+
+class Program : NewBaseType
+{
+}
