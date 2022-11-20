af1bafbf4fe803a9c511164e61e2346fac38f671
alexanderholguin <118689700+alexanderholguin@users.noreply.github.com>
2022-11-20 15:39:20 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        float n3;
+        float n4;
+        Console.WriteLine("ingrese un numero ");
+        n3 = float.Parse(Console.ReadLine());
+
+        Console.WriteLine("ingrese un numero :");
+        n4 = float.Parse(Console.ReadLine());
+        if ( n3 > n4 ){
+            Console.WriteLine("el numero mayor es " + n3);
+        }
+        
+
+    }
+}
