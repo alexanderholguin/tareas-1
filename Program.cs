53179005092ac09bbb012ace49fdc0128401eff7
alexanderholguin <118689700+alexanderholguin@users.noreply.github.com>
2022-11-20 17:33:30 -0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,33 +1,21 @@
-﻿/*  jonathan holguin baque 3er A  */
-class Program
+﻿internal class NewBaseType
 {
-    static void Main (string[] args)
+    static void Main(string[] args)
     {
-        int n1, n2, n3;
-        Console.WriteLine("escriba un numero:");
-        n1 = int.Parse(Console.ReadLine());
+        int num = 0;
+        int suma = 0;
 
-        Console.WriteLine("escriba un numero:");
-        n2 = int.Parse(Console.ReadLine());
-
-        Console.WriteLine("escriba un numero:");
-        n3 = int.Parse(Console.ReadLine());
-
-        int mayor , menor ;
-
-        mayor = n1;
-
-        if (mayor <n2 ) mayor =n2;
-        if (mayor < n3) mayor = n3;
-
-        menor = n1;
-
-        if (menor >n2 ) menor = n2;
-        if (menor >n3 ) menor = n3;
-
-        Console.WriteLine("el mayor de los tres numeros es:" + mayor);
-        Console.WriteLine("el menor de los tres numeros es : " + menor );
-
-        Console.ReadLine();
+        do
+        {
+            Console.WriteLine("ingresar un numero:");
+            num = int.Parse(Console.ReadLine());
+            suma = suma + num;
+        } while (num != 0);
+        Console.WriteLine("la suma es:" + suma);
+        Console.ReadKey();
     }
+}
+
+class Program : NewBaseType
+{
 }
