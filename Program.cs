38e1bd64414ee82669d91d9acd3b4449bb6aff50
alexanderholguin <118689700+alexanderholguin@users.noreply.github.com>
2022-11-20 17:15:30 -0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,20 +1,33 @@
-﻿/* jonathan holguin baque 3ero A  */
-
+﻿/*  jonathan holguin baque 3er A  */
 class Program
 {
-    static void Main(string[] args)
+    static void Main (string[] args)
     {
-        //determinar si un numero es par 
-        Console.Write("introducir el primer numero: ");
-        double numero = Convert.ToDouble(Console.ReadLine());
-        if(numero % 2 == 0)
-        {
-            Console.Write("el numero" + numero + "es par.");
-        }
-        else
-        {
-            Console.Write("el numero " + numero + " es impar.");
-        }
+        int n1, n2, n3;
+        Console.WriteLine("escriba un numero:");
+        n1 = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("escriba un numero:");
+        n2 = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("escriba un numero:");
+        n3 = int.Parse(Console.ReadLine());
+
+        int mayor , menor ;
+
+        mayor = n1;
+
+        if (mayor <n2 ) mayor =n2;
+        if (mayor < n3) mayor = n3;
+
+        menor = n1;
+
+        if (menor >n2 ) menor = n2;
+        if (menor >n3 ) menor = n3;
+
+        Console.WriteLine("el mayor de los tres numeros es:" + mayor);
+        Console.WriteLine("el menor de los tres numeros es : " + menor );
+
         Console.ReadLine();
     }
 }
