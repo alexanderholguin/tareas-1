d5058be27b2e5196a594f93b5799d27306327c29
alexanderholguin <118689700+alexanderholguin@users.noreply.github.com>
2022-11-20 16:09:14 -0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,18 +1,2 @@
-﻿internal class Program
-{
-    private static void Main(string[] args)
-    {
-        float n3;
-        float n4;
-        Console.WriteLine("ingrese un numero ");
-        n3 = float.Parse(Console.ReadLine());
-
-        Console.WriteLine("ingrese un numero :");
-        n4 = float.Parse(Console.ReadLine());
-        if ( n3 > n4 ){
-            Console.WriteLine("el numero mayor es " + n3);
-        }
-        
-
-    }
-}
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
